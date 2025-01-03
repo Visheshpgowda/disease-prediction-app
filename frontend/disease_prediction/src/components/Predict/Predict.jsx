604035f66a382c6a3d97b2d3c42475aac190bb19
Somanath Mikali <somanathmikali@Somanaths-MacBook-Air.local>
2025-01-03 23:25:18 +0530

    some deployed

--- frontend/disease_prediction/src/components/Predict/Predict.jsx
+++ frontend/disease_prediction/src/components/Predict/Predict.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
  
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const Predict = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [formData, setFormData] = useState({});
