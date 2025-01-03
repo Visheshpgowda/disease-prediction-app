3099f6133b19947c73513427a16ab9f9dd0acab0
Somanath Mikali <somanathmikali@Somanaths-MacBook-Air.local>
2025-01-03 21:40:05 +0530

    done

--- frontend/disease_prediction/src/components/AiBot/AiChatbot.jsx
+++ frontend/disease_prediction/src/components/AiBot/AiChatbot.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AiChatbot = () => {
     return (
         <div className='h-screen w-screen flex items-center justify-center mt-10 mb-10'>
-            <iframe src="https://bot.orimon.ai/?tenantId=4e52b316-a151-46df-baee-bb3e90f398bc&fullScreenBot=true" height="100%" width="90%" frameBorder="0" border="none"></iframe>
+             <iframe src="https://bot.orimon.ai/?tenantId=328988cd-3f86-490d-a7d7-64a04513865d&fullScreenBot=true" height="100%" width="100%" frameBorder="0" border="none"></iframe> 
         </div>
     )
 }
